a28b942b2d8c076e165fc596d8b519c53d11f786
jennisun <jennifersun@Jennifers-MacBook-Air-2.local>
2020-10-29 15:38:02 -0400

    distance to

---
+++ Tester.java
@@ -0,0 +1,9 @@
+public class Tester {
+  public static void main (String[] args) {
+    Point p1 = new Point (1, 2);
+    Point p2 = new Point (1, 3);
+
+    System.out.println(p1.distanceTo(p2));
+  }
+
+}
