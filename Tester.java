a36ecb01e1ac1c95aae0e21cc6b469d567a2783b
jennisun <jennifersun@Jennifers-MacBook-Air-2.local>
2020-10-29 22:14:26 -0400

    done

--- Tester.java
+++ Tester.java
@@ -1,17 +1,17 @@
 public class Tester {
   public static void main (String[] args) {
-    Point p1 = new Point (1, 2);
-    Point p2 = new Point (4, 3);
-    Point p3 = new Point (3, 3);
+    Point p1 = new Point (0, 0);
+    Point p2 = new Point (4, 0);
+    Point p3 = new Point (2, 2);
 
     Triangle t1 = new Triangle(p1, p2, p3);
 
-    // System.out.println(p1.distanceTo(p2));
-    // System.out.println(p2.equals(p3));
-    // System.out.println(p1.equals(p3));
 
+    System.out.println(t1.classify());
+    System.out.println(t1.toString());
 
-    System.out.println(t1.getArea());
+    t1.setVertex(2, new Point(4, 0));
+    System.out.println(t1.toString());
 
   }
 
