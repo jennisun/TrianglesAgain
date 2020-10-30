4bb041e1913c4fa654bb8f5f2ffb791da17fd011
jennisun <jennifersun@Jennifers-MacBook-Air-2.local>
2020-10-29 21:03:43 -0400

    HELLO

--- Tester.java
+++ Tester.java
@@ -1,9 +1,18 @@
 public class Tester {
   public static void main (String[] args) {
     Point p1 = new Point (1, 2);
-    Point p2 = new Point (1, 3);
+    Point p2 = new Point (4, 3);
+    Point p3 = new Point (3, 3);
 
-    System.out.println(p1.distanceTo(p2));
+    Triangle t1 = new Triangle(p1, p2, p3);
+
+    // System.out.println(p1.distanceTo(p2));
+    // System.out.println(p2.equals(p3));
+    // System.out.println(p1.equals(p3));
+
+
+    System.out.println(t1.getArea());
+
   }
 
 }
